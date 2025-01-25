1f9a64b6bf19f21dd958e8f9e8106279ca940051
Jack Pena <penajackc@gmail.com>
2025-01-25 11:23:06 -0600

    initial implementation w vanilla javascript and tailwind

--- assets/js/app.js
+++ assets/js/app.js
@@ -21,6 +21,7 @@ import "phoenix_html"
 import {Socket} from "phoenix"
 import {LiveSocket} from "phoenix_live_view"
 import topbar from "../vendor/topbar"
+import Chat from "./chat"
 
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
 let liveSocket = new LiveSocket("/live", Socket, {
@@ -35,6 +36,7 @@ window.addEventListener("phx:page-loading-stop", _info => topbar.hide())
 
 // connect if there are any LiveViews on the page
 liveSocket.connect()
+Chat.init()
 
 // expose liveSocket on window for web console debug logs and latency simulation:
 // >> liveSocket.enableDebug()
